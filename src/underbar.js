cd0edf9a1fc492aea159e68af13a45bd92610042
Orlando Castillo <orlandoc@sas.upenn.edu>
2015-11-15 21:54:25 -0500

    completed uniq, map, and reduce functions

--- src/underbar.js
+++ src/underbar.js
@@ -94,11 +94,18 @@
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
-    return _.filter(collection, function(item) { return !test(item);});
+    return _.filter(collection, function(item) { return !test(item);} );
   };
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+    var final = [];
+
+    _.each(array, function(item) {
+      if(_.indexOf(final,item) == -1) final.push(item);
+    });
+
+    return final;
   };
 
 
@@ -107,6 +114,11 @@
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var final = [];
+    
+    _.each(collection, function(item) {final.push(iterator(item));} );
+
+    return final;
   };
 
   /*
@@ -148,6 +160,13 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+      if(accumulator == null) accumulator = collection.shift();
+
+      _.each(collection, function(item) {
+        accumulator = iterator(accumulator, item);
+      });
+
+      return accumulator;
   };
 
   // Determine if the array or object contains a given value (using `===`).
